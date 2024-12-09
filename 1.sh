1133febe104bd54cf6ed48492dd60c7defb440be
shubham <shubhammishra4227@gmail.com>
2024-12-09 13:52:12 +0530

    dded  -f

--- 1.sh
+++ 1.sh
@@ -4,5 +4,7 @@ sudo apt install nginx -y
 systemctl enable nginx
 systemctl start nginx
 systemctl status nginx > nginx.log
-echo "hello world from $( " 
+echo "hello world from $(hostname -f) "
+echo $?
+
 
